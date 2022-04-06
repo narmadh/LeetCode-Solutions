263f852d81b43d1b36cf4d0dacc83193367f395a
Narmadha M <79155752+narmadh@users.noreply.github.com>
2022-04-06 15:41:43 +0530

    Time: 436 ms (5.07%), Space: 42.3 MB (6.16%) - LeetHub

--- 344-reverse-string/344-reverse-string.py
+++ 344-reverse-string/344-reverse-string.py
@@ -7,7 +7,7 @@ class Solution:
         n=len(s)
         self.reverseString2(p1,n,s)
     def reverseString2(self,p1,n,s):
-        if(p1>=n-p1-1):
+        if(p1>=n/2):
             return s
         s[p1],s[n-p1-1]=s[n-p1-1],s[p1]
         self.reverseString2(p1+1,n,s)
