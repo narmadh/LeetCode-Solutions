eaf7052d84f69ad7512206a88b33f48a9b0f0714
Narmadha M <79155752+narmadh@users.noreply.github.com>
2022-04-06 15:32:46 +0530

    Time: 343 ms (19.19%), Space: 43.2 MB (5.35%) - LeetHub

--- 344-reverse-string/344-reverse-string.py
+++ 344-reverse-string/344-reverse-string.py
@@ -5,8 +5,10 @@ class Solution:
         """
         p1=0
         p2=len(s)-1
-        while(p1<p2):
-            s[p1],s[p2]=s[p2],s[p1]
-            p1+=1
-            p2-=1
+        self.reverseString2(p1,p2,s)
+    def reverseString2(self,p1,p2,s):
+        if(p1>=p2):
+            return s
+        s[p1],s[p2]=s[p2],s[p1]
+        self.reverseString2(p1+1,p2-1,s)
         
