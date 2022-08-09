3bc79adf16ffdd92c7cf2ceff003df1868c50db2
Narmadha M <79155752+narmadh@users.noreply.github.com>
2022-08-09 13:14:49 +0530

    Time: 49 ms (47.57%), Space: 13.9 MB (60.12%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.py
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.py
@@ -6,16 +6,44 @@
 #         self.right = right
 class Solution:
     def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        if not root:
+        if root is None:
             return root
-        stack1=[root]
-        stack2=[]
-        while(stack1):
-            node=stack1.pop(-1)
-            stack2.append(node.val)
-            if node.left:
-                stack1.append(node.left)
-            if node.right:
-                stack1.append(node.right)
-        stack2.reverse()
-        return stack2
+        def peek(stack):
+            if len(stack) > 0:
+                return stack[-1]
+            return None
+        ans=[]
+        stack = []
+     
+        while(True):
+         
+            while (root):
+            # Push root's right child and then root to stack
+                if root.right is not None:
+                    stack.append(root.right)
+                stack.append(root)
+ 
+            # Set root as root's left child
+                root = root.left
+         
+        # Pop an item from stack and set it as root
+            root = stack.pop()
+ 
+        # If the popped item has a right child and the
+        # right child is not processed yet, then make sure
+        # right child is processed before root
+            if (root.right is not None and
+                peek(stack) == root.right):
+                stack.pop() # Remove right child from stack
+                stack.append(root) # Push root back to stack
+                root = root.right # change root so that the
+                            # right childis processed next
+ 
+        # Else print root's data and set root as None
+            else:
+                ans.append(root.val)
+                root = None
+ 
+            if (len(stack) <= 0):
+                break
+        return ans
