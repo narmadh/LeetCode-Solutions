7f2813a38d44d0a94f4e8acbeff2977be33dd950
Narmadha M <79155752+narmadh@users.noreply.github.com>
2022-09-25 18:29:57 +0530

    Time: 483 ms (15.30%), Space: 18.5 MB (21.04%) - LeetHub

--- 344-reverse-string/344-reverse-string.py
+++ 344-reverse-string/344-reverse-string.py
@@ -3,12 +3,11 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        p1=0
-        n=len(s)
-        self.reverseString2(p1,n,s)
-    def reverseString2(self,p1,n,s):
-        if(p1>=n/2):
-            return s
-        s[p1],s[n-p1-1]=s[n-p1-1],s[p1]
-        self.reverseString2(p1+1,n,s)
+        i=0
+        j=len(s)-1
+        while(i<j):
+            s[i],s[j]=s[j],s[i]
+            i+=1
+            j-=1
+        return s
         
