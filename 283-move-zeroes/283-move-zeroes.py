bf20f744f3c75ee8b8c57994b187f92a7ccb196e
Narmadha M <79155752+narmadh@users.noreply.github.com>
2022-10-04 13:04:05 +0530

    Time: 285 ms (62.17%), Space: 15.6 MB (64.49%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.py
+++ 283-move-zeroes/283-move-zeroes.py
@@ -3,12 +3,8 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        if len(nums)==1:
-            return nums
-        n=0
+        zero = 0  # records the position of "0"
         for i in range(len(nums)):
-            if nums[i]==0:
-                n+=1
-            elif nums[i]!=0  and n!=0:
-                nums[i-n]=nums[i]
-                nums[i]=0
+            if nums[i] != 0:
+                nums[i], nums[zero] = nums[zero], nums[i]
+                zero += 1
